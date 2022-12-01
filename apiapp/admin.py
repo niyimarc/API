9b3b9f2ed6dd2abd3fde4158cdb9360edcb7e3a7
oluwadamilareadeniye@yahoo.com <oluwadamilareadeniye@yahoo.com>
2022-12-02 02:05:09 +0800

    Created the PostCategory and Blogpost model

--- apiapp/admin.py
+++ apiapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BlogPost, PostCategory
 
 # Register your models here.
+
+admin.site.register((BlogPost, PostCategory))
--- apiapp/models.py
+++ apiapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+# Create a model for the post categories 
+class PostCategory(models.Model):
+    category_name = models.CharField(max_length = 150, verbose_name = 'Category name')
+    category_description = models.TextField(verbose_name = 'Description', blank = True, null = True)
+    def __str__(self):
+        return self.category_name
+
+
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+# create a model to post on blog 
+class BlogPost(models.Model):
+    post_title = models.CharField(max_length =150, verbose_name = 'Post Title')
+    post_category = models.ForeignKey(PostCategory, on_delete = models.CASCADE, verbose_name = 'Post Category')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    post_contents = models.TextField()
+    class Meta:
+        ordering  = ['-created_date']
+    def __str__(self):
+        return self.post_title
+
