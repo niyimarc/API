588fdff4aff34c9305de67adbc869f4b9db3f749
oluwadamilareadeniye@yahoo.com <oluwadamilareadeniye@yahoo.com>
2022-12-03 07:08:13 +0800

    used viewsets from rest_framework to set up the API

--- apiapp/views.py
+++ apiapp/views.py
@@ -2,16 +2,10 @@ from django.http import JsonResponse
 from rest_framework import generics
 from .models import BlogPost
 from .serializers import TestApiSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 
-class GenericTestApi(generics.ListCreateAPIView):
+class ViewsetTestApi(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = TestApiSerializer
-
-class GenericTestApiUpdate(generics.UpdateAPIView):
-    queryset = BlogPost.objects.all()
-    serializer_class = TestApiSerializer
-    # we can specify our own lookup_field 
-    # if we dont want to use the pk in the url 
-    lookup_field = "id"
--- apiproject/urls.py
+++ apiproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from apiapp.views import GenericTestApi, GenericTestApiUpdate
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from apiapp.views import ViewsetTestApi
+
+router = DefaultRouter()
+router.register("api", ViewsetTestApi)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api', GenericTestApi.as_view()),
-    # path('generic/<int:pk>', GenericTestApiUpdate.as_view()), 
-    path('api/<int:id>', GenericTestApiUpdate.as_view()),
+    path("", include(router.urls)),
 ]
